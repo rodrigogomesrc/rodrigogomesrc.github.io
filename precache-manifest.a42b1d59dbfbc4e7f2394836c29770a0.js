e2267d830333658630499b6a9899a705ccabd5ac
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2020-01-08 18:33:05 -0300

    deply static

---
+++ precache-manifest.a42b1d59dbfbc4e7f2394836c29770a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fde424ad34e90d96b11fbcdbc2f63d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b46f4bb474b8f117a101",
+    "url": "/static/css/main.14a820a0.chunk.css"
+  },
+  {
+    "revision": "bf9b9e1f38e29d09e285",
+    "url": "/static/js/2.758ef0d9.chunk.js"
+  },
+  {
+    "revision": "aadf14a4267279553d78d5c0fdf83f74",
+    "url": "/static/js/2.758ef0d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b46f4bb474b8f117a101",
+    "url": "/static/js/main.56174987.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "4fe473edd7c0d2781398f583edaad47d",
+    "url": "/static/media/geneticats.4fe473ed.png"
+  },
+  {
+    "revision": "f895aed7f38a3d6f71b87829d552f424",
+    "url": "/static/media/picture.f895aed7.png"
+  }
+]);
