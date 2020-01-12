94a4867c1d14c697eda6c616dbd81c4e2c972558
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2020-01-12 16:57:25 -0300

    deploy

---
+++ precache-manifest.10b3548843691ed9fa14e7d614276942.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d39957d155763a217bf781c9688c1d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1cb8d0f8b2901cda0f5",
+    "url": "/static/css/main.b11eb13e.chunk.css"
+  },
+  {
+    "revision": "e9dd312613cf1587aed2",
+    "url": "/static/js/2.8262dc4f.chunk.js"
+  },
+  {
+    "revision": "aadf14a4267279553d78d5c0fdf83f74",
+    "url": "/static/js/2.8262dc4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1cb8d0f8b2901cda0f5",
+    "url": "/static/js/main.ab049214.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "4fe473edd7c0d2781398f583edaad47d",
+    "url": "/static/media/geneticats.4fe473ed.png"
+  },
+  {
+    "revision": "f895aed7f38a3d6f71b87829d552f424",
+    "url": "/static/media/picture.f895aed7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65b28d516fe9cb844b6d67d31ec398a7.js"
+  "/precache-manifest.10b3548843691ed9fa14e7d614276942.js"
 );
 
 self.addEventListener('message', (event) => {
