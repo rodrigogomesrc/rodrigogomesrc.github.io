c3598486909576b78a2f1e956f012fca2b1273a8
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-02-03 19:27:04 -0300

    update profile picture and some other configs

---
+++ precache-manifest.ae93ebadabd2d384e75b7d5bc40d6bc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da80502af1d70b0d8b2f75ef045dfe88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a6c593abf5e430ed461",
+    "url": "/static/css/main.50900d85.chunk.css"
+  },
+  {
+    "revision": "d35f1975ae9a7cb69d53",
+    "url": "/static/js/2.21216127.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.21216127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6c593abf5e430ed461",
+    "url": "/static/js/main.5c9ead35.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "70d74c72c4be371d8e57ffc49d11cc20",
+    "url": "/static/media/geneticats.70d74c72.png"
+  },
+  {
+    "revision": "91c7e1a8d2dea2a801cbe0b304f6ba80",
+    "url": "/static/media/picture.91c7e1a8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cae3cd4234f3b076f519efd347d5955.js"
+  "/precache-manifest.ae93ebadabd2d384e75b7d5bc40d6bc6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
